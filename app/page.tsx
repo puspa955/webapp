a8d06923dbae92788cd62bee52e1d153fc9bbe3d
puspa <limbup503@gmail.com>
2024-04-10 11:56:32 +0545

    oo

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-screen w-full">
+      
         
         <div className='absolute right-0 left-0 z-10'>
           <LandingPageNav section1Id="section1" section2Id="section2" section3Id="section3" section4Id="section4" section5Id="section5" />
@@ -45,7 +45,7 @@ export default function Home() {
           </Link>
         </button>
         </div>
-       </div>
+       
        
       <div id="section1">
         <About />
