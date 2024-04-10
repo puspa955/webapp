464f85d8f51e8d1d4e45d3e30bf2961d9a5dbfae
puspa <limbup503@gmail.com>
2024-04-10 20:03:15 +0545

    e

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Mentors from './components/Mentors';
 import Team from './components/Team';
 import LandingPageNav from './components/LandingPageNav';
 import Background from '../public/images/bg.jpg';
+import TopDestination from './components/TopDestination';
 import Image from 'next/image';
 
 import Link from 'next/link';
@@ -26,7 +27,7 @@ export default function Home() {
 
         {/* Navigation */}
         <div className='absolute top-0 right-0 z-20'>
-          <LandingPageNav section1Id="section1" section2Id="section2" section3Id="section3" section4Id="section4" section5Id="section5" />
+          <LandingPageNav section1Id="section1" section2Id="section2" section3Id="section3" section4Id="section4" section5Id="section5" section6Id="section6" />
         </div>
 
         
@@ -50,6 +51,9 @@ export default function Home() {
       {/* Sections */}
       <div id="section1">
         <About />
+      </div>
+      <div id="section6">
+        <TopDestination />
       </div>
       <div id="section2">
         <Research />
